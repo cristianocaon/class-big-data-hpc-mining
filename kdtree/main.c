12b16209a64a2a6a142696da973995924ee712fe
Cristiano Caon <cristiano.e.caon@gmail.com>
2022-09-27 16:14:27 -0500

    fixed bdry and assign

--- kdtree/main.c
+++ kdtree/main.c
@@ -7,53 +7,74 @@ int bipartition(int dim, int i0, int im, double* data, int chosen_dim, int clust
     double* cluster_bdry[2], double cluster_centroid,                                       // output
     short* cluster_assign) {                                                                // buffer
 
-    double* bdry_buffer = malloc(sizeof(double) * 2);
+    // Buffer becomes the bi-partitioned data array for current two clusters.
     double* buffer = malloc(sizeof(double) * im * dim);
 
-    printf("\ni0: %d\tim: %d\tdim: %d\tchosen_dim: %d\tcentroid: %f\n\n", i0, im, dim, chosen_dim, cluster_centroid);
+    printf("\ni0: %d\tim: %d\tdim: %d\tchosen_dim: %d\tcentroid: %f\tcluster ids: %d, %d\n\n", i0, im, dim, chosen_dim, cluster_centroid, cluster_id, (cluster_id + 1));
 
-    int ii = 0, jj = im * dim, start_assign = i0 / dim, end_assign = (i0 / dim) + im - 1;
+    int ii = 0,
+        jj = im * dim,
+        start_assign = i0 / dim,
+        end_assign = (i0 / dim) + im - 1,
+        is_left_first = dim,
+        is_right_first = dim;
 
     for (int i = i0 + chosen_dim; i < im * dim + i0; i += dim) {
         if (data[i] <= cluster_centroid) {
-            // Assigning current data point to left cluster.
-            cluster_assign[start_assign] = cluster_id;
 
-            int bdry_index = 0;
+            int left_bdry_idx = 0;
             for (int j = i - chosen_dim; j < i - chosen_dim + dim; j++) {
+                // Update left cluster min and max boundaries in first occurrence.
+                if (is_left_first > 0) {
+                    cluster_bdry[cluster_id][left_bdry_idx] = data[j];
+                    cluster_bdry[cluster_id][left_bdry_idx + 1] = data[j];
+                    is_left_first -= 1;
+                }
                 // Updating left cluster min boundaries.
-                if (data[j] < cluster_bdry[cluster_id][bdry_index]) {
-                    cluster_bdry[cluster_id][bdry_index] = data[j];
+                else if (data[j] < cluster_bdry[cluster_id][left_bdry_idx]) {
+                    cluster_bdry[cluster_id][left_bdry_idx] = data[j];
                 }
                 // Updating left cluster max boundaries.
-                if (data[j] > cluster_bdry[cluster_id][bdry_index + 1]) {
-                    cluster_bdry[cluster_id][bdry_index + 1] = data[j];
+                else if (data[j] > cluster_bdry[cluster_id][left_bdry_idx + 1]) {
+                    cluster_bdry[cluster_id][left_bdry_idx + 1] = data[j];
                 }
+
                 buffer[ii] = data[j];
+                left_bdry_idx += 2;
                 ii += 1;
-                bdry_index += 2;
             }
+
+            // Assigning current data point to left cluster.
+            cluster_assign[start_assign] = cluster_id;
+            start_assign += 1;
         }
         else {
+
+            int right_bdry_idx = (dim * 2) - 1;
+            for (int j = i - chosen_dim + dim - 1; j >= i - chosen_dim; j--) {
+                // Update left cluster min and max boundaries in first occurrence.
+                if (is_right_first > 0) {
+                    cluster_bdry[cluster_id + 1][right_bdry_idx - 1] = data[j];
+                    cluster_bdry[cluster_id + 1][right_bdry_idx] = data[j];
+                    is_right_first -= 1;
+                }
+                // Updating right cluster min boundaries.
+                else if (data[j] < cluster_bdry[cluster_id + 1][right_bdry_idx - 1]) {
+                    cluster_bdry[cluster_id + 1][right_bdry_idx - 1] = data[j];
+                }
+                // Updating right cluster max boundaries.
+                else if (data[j] > cluster_bdry[cluster_id + 1][right_bdry_idx]) {
+                    cluster_bdry[cluster_id + 1][right_bdry_idx] = data[j];
+                }
+
+                buffer[jj - 1] = data[j];
+                right_bdry_idx -= 2;
+                jj -= 1;
+            }
+
             // Assigning current data point to right cluster.
             cluster_assign[end_assign] = cluster_id + 1;
             end_assign -= 1;
-
-            int bdry_index = (dim - 1) * 2;
-            for (int j = i - chosen_dim + dim - 1; j >= i - chosen_dim; j--) {
-                // Updating right cluster min boundaries.
-                if (data[j] < cluster_bdry[cluster_id][bdry_index - 1]) {
-                    cluster_bdry[cluster_id][bdry_index - 1] = data[j];
-                }
-                // Updating right cluster max boundaries.
-                if (data[j] > cluster_bdry[cluster_id][bdry_index]) {
-                    cluster_bdry[cluster_id][bdry_index] = data[j];
-                }
-                buffer[jj - 1] = data[j];
-                jj -= 1;
-                bdry_index -= 2;
-
-            }
         }
     }
 
@@ -70,12 +91,7 @@ int bipartition(int dim, int i0, int im, double* data, int chosen_dim, int clust
         ii += 1;
     }
 
-    // for (int i = 0; i < 10; i++) {
-    //     printf("%d\n", cluster_assign[i]);
-    // }
-
     free(buffer);
-    free(bdry_buffer);
 
     return 0;
 }
@@ -94,25 +110,16 @@ int kd_tree(int dim, int ndata, double* data, int kk) {
         cluster_centroid[kk][dim]
     *************************************************************************************/
 
-    int* cluster_start = malloc(sizeof(int) * kk);
     int* cluster_size = malloc(sizeof(int) * kk);
+    int* cluster_start = malloc(sizeof(int) * kk);
+
+    short* cluster_assign = malloc(sizeof(short) * ndata);
 
     double** cluster_bdry = malloc(sizeof(double*) * kk * 2 * dim);
     double** cluster_centroid = malloc(sizeof(double*) * kk * dim);
 
-    short* cluster_assign = malloc(sizeof(short) * ndata);
-
     // Populating cluster_start & cluster_size
     cluster_start[0] = 0, cluster_size[0] = ndata;
-    for (int i = 1; i < kk; i++) {
-        cluster_start[i] = -1;
-        cluster_size[i] = -1;
-    }
-
-    // Populating cluster_assign
-    for (int i = 0; i < ndata; i++) {
-        cluster_assign[i] = 0;
-    }
 
     // Populating cluster_bdry
     for (int i = 0; i < kk; i++) {
@@ -125,6 +132,7 @@ int kd_tree(int dim, int ndata, double* data, int kk) {
                 temp_bdry[j] = __DBL_MIN__;
             }
         }
+        cluster_bdry[i] = temp_bdry;
     }
 
     double* centroid = malloc(sizeof(double) * dim);        // Temporarily stores centroid for current cluster.
@@ -137,20 +145,17 @@ int kd_tree(int dim, int ndata, double* data, int kk) {
         int* c_start_buffer = malloc(sizeof(int) * kk);
         int* c_size_buffer = malloc(sizeof(int) * kk);
 
-        for (int i = 0; i < kk; i++) {
-            c_start_buffer[i] = -1;
-            c_size_buffer[i] = -1;
-        }
-
         for (int j = 0; j < jj; j++) {                      // j loops through indices of all jj clusters.
 
             int chosen_dim = -1;
             double largest_variance = __DBL_MIN__;
 
             for (int i = 0; i < dim; i++) {
+
                 double sum = 0.0;
                 int k = 0;
-                for (int ii = cluster_start[j] + i; ii < cluster_size[j] * dim + cluster_start[j]; ii += dim) {
+
+                for (int ii = cluster_start[j] + i; ii < cluster_start[j] + cluster_size[j] * dim; ii += dim) {
                     buffer[k] = data[ii];
                     sum += data[ii];
                     k += 1;
@@ -167,7 +172,7 @@ int kd_tree(int dim, int ndata, double* data, int kk) {
                 // Variance calculation for current dimension
                 double dim_variance = variance_numerator / k;
 
-                // printf("Current dimension: %d\tVariance: %f\tMean: %f\n", i, dim_variance, centroid[i]);
+                printf("Dimension %d --> Variance: %f, Mean: %f\n", i, dim_variance, centroid[i]);
 
                 // Determines dimension with largest variance
                 if (dim_variance > largest_variance) {
@@ -178,8 +183,6 @@ int kd_tree(int dim, int ndata, double* data, int kk) {
 
             cluster_centroid[j] = centroid;
 
-            printf("\nLargest variance is from dimension %d: %f\n", chosen_dim, largest_variance);
-
             int cluster_id = (j == 0) ? j : j + 1;
 
             //   call bipartition() to partition the j-th cluster into 2 clusters
@@ -187,28 +190,39 @@ int kd_tree(int dim, int ndata, double* data, int kk) {
                 c_start_buffer, c_size_buffer, cluster_bdry, cluster_centroid[j][chosen_dim], cluster_assign);
 
 
-            printf("Partition result:\n");
+            printf("Bipartition result:\n");
             for (int i = 0; i < ndata * dim; i++) {
                 printf("%f\t", data[i]);
                 if ((i + 1) % dim == 0) {
                     printf("\n");
                 }
             }
+            printf("\n");
 
         }
-
-        // for (int i = 0; i < kk; i++) {
-        //     printf("Cluster %d --> Start: %d\tSize: %d\n", i, cluster_start[i], cluster_size[i]);
-        // }
-
-        for (int i = 0; i < kk; i++) {
-            printf("Min %f\tMax: %f\n", cluster_bdry[i][0], cluster_bdry[i][1]);
-        }
-
-        printf("\n+-----------------------------------------------+\n\n");
 
         cluster_start = c_start_buffer;
         cluster_size = c_size_buffer;
+
+        printf("\n");
+
+        for (int i = 0; i < kk; i++) {
+            printf("Cluster %d --> Start: %d\tSize: %d\n", i, cluster_start[i], cluster_size[i]);
+        }
+
+        printf("\n");
+
+        // for (int i = 0; i < jj * 2; i++) {
+        //     for (int j = 0; j < dim * 2; j += 2) {
+        //         printf("Cluster %d / Dimension %d --> Min %f, Max: %f\n", i, j / 2, cluster_bdry[i][j], cluster_bdry[i][j + 1]);
+        //     }
+        // }
+
+        for (int i = 0; i < ndata; i++) {
+            printf("%d\n", cluster_assign[i]);
+        }
+
+        printf("\n+-----------------------------------------------+\n\n");
 
         jj *= 2;
     }
@@ -234,10 +248,11 @@ int kd_tree(int dim, int ndata, double* data, int kk) {
 
 int main() {
 
-    int ndata = 10, dim = 6, kk = 4;
+    int ndata = 10, dim = 3, kk = 4;
     double* data = malloc(sizeof(double) * ndata * dim);
 
     // Generating data
+    printf("\n+------------------------- Original data -------------------------+\n");
     for (int i = 0; i < ndata * dim; i++) {
         data[i] = (double)rand() / RAND_MAX;
         printf("%f\t", data[i]);
@@ -246,7 +261,8 @@ int main() {
             printf("\n");
         }
     }
-    printf("\n");
+    printf("+-----------------------------------------------------------------+\n\n");
+
 
     kd_tree(dim, ndata, data, kk);
 
